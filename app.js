cb9d69310abe7bbe2136c34244e8fa36f7fa343c
unknown <>
2016-12-06 10:38:52 +0900

    first commit

---
+++ app.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const app = express();
+
+var http = require('http').Server(app);
+
+var io = require('socket.io')(http);
+
+app.use(express.static('public'));
+
+app.get('/', function(req, res) {
+  res.sendfile('index.html');
+});
+
+io.on('connection', function(socket){
+  console.log('a user connected');
+});
+
+const port = process.env.PORT|| 3000;
+http.listen(port, function() {
+  console.log("Server listening at localhost:" + port);
+});
