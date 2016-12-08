807f4872458304d3d3521ce67049a4516f0e67d6
Shunsuke Sasaki <omu.omugin@gmail.com>
2016-12-09 00:53:59 +0900

    upload server

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ const io = require('socket.io')(http);
 var fs = require('fs');
 var multer = require('multer');
 var bodyParser = require('body-parser');
+var sharp = require('sharp');
 
 
 app.set('views', __dirname + '/views');
@@ -30,11 +31,23 @@ app.get('/upload', function(req, res) {
 app.post('/upload', function(req, res) {
   var temp_path = req.file.path;
   var target_path = "public/images/" + req.file.originalname;
+  var resized_path = "public/resized_images/" + req.file.originalname;
+
   console.log("temp: " + temp_path);
   console.log("target: " + target_path);
+  console.log("reszied: " + resized_path);
+
   fs.rename(temp_path, target_path, function(err) {
     if (err) throw err;
     console.log('filename successfully changed');
+  });
+
+  sharp(target_path)
+    .resize(300, 300)
+    .toFile(resized_path, function(err) {
+    // output.jpg is a 200 pixels wide and 200 pixels high image
+    // containing a scaled and cropped version of input.jpg
+    if (err) throw err;
     refresh()
   });
   res.render('upload');
@@ -44,7 +57,7 @@ io.sockets.on('connection', function(socket) {
 
   socket.on('client_ready', function() {
     console.log('NEW CLIENT');
-    fs.readdir('./public/images', function(err, files) {
+    fs.readdir('./public/resized_images', function(err, files) {
       if (err) throw err;
       socket.emit('client_ready_back', files);
     });
@@ -69,7 +82,7 @@ http.listen(port, function() {
 });
 
 function refresh() {
-  fs.readdir('./public/images', function(err, files) {
+  fs.readdir('./public/resized_images', function(err, files) {
     if (err) throw err;
     io.sockets.emit('refresh', files);
   });
